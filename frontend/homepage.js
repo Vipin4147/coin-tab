7646759ff32e0573d1f520d96ddc1127400fc9c9
Vipin4147 <vipin4147@gmail.com>
2024-02-15 10:48:13 +0530

    home page created

---
+++ frontend/homepage.js
@@ -0,0 +1,39 @@
+let user = document.getElementById("users");
+
+user.onclick = async () => {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await res.json();
+    console.log(data);
+    append_data(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let append_data = (data) => {
+  let container = document.getElementById("container");
+  container.innerHTML = null;
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    let name = document.createElement("p");
+    name.innerText = "Name" + " : " + el.name;
+    let email = document.createElement("p");
+    email.innerText = "Email" + " : " + el.email;
+    let phone = document.createElement("p");
+    phone.innerText = "Phone" + " : " + el.phone;
+    let website = document.createElement("p");
+    website.innerText = "Website" + " : " + el.website;
+    let city = document.createElement("p");
+    city.innerText = "City" + " : " + el.address.city;
+    let company = document.createElement("p");
+    company.innerText = "Company" + " : " + el.company.name;
+
+    let open = document.createElement("button");
+    open.innerText = "Open";
+    let add = document.createElement("button");
+    add.innerText = "Add";
+    div.append(name, email, phone, website, city, company, open, add);
+    container.append(div);
+  });
+};
